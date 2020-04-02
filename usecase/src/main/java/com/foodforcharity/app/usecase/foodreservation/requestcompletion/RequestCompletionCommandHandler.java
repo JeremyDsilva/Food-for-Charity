702b0580da0a16ff65bd85303d00dbf7a0b01998
PriyankaPrakashChand <53374732+PriyankaPrakashChand@users.noreply.github.com>
2020-04-02 21:14:20 +0400

    Request completion use case version1

--- usecase/src/main/java/com/foodforcharity/app/usecase/foodreservation/requestcompletion/RequestCompletionCommandHandler.java
+++ usecase/src/main/java/com/foodforcharity/app/usecase/foodreservation/requestcompletion/RequestCompletionCommandHandler.java
@@ -1,25 +1,84 @@
 package com.foodforcharity.app.usecase.foodreservation.requestcompletion;
 
-import com.foodforcharity.app.mediator.CommandHandler;
+import java.util.Optional;
 
+import com.foodforcharity.app.domain.constant.Error;
+import com.foodforcharity.app.domain.constant.DonorStatus;
+import com.foodforcharity.app.domain.entity.Donor;
+import com.foodforcharity.app.domain.entity.Request;
+import com.foodforcharity.app.domain.reponse.Response;
+import com.foodforcharity.app.mediator.CommandHandler;
+import com.foodforcharity.app.service.ComplaintRepository;
+import com.foodforcharity.app.service.RequestRepository;
 
 import org.springframework.stereotype.Service;
 
 @Service
-public class RequestCompletionCommandHandler implements CommandHandler<RequestCompletionCommand, Void> {
-	
+public class RequestCompletionCommandHandler implements CommandHandler<RequestCompletionCommand, Response<Void>> {
+	ComplaintRepository complaintRepository;
+	RequestRepository requestRepository;
 
 	public RequestCompletionCommandHandler() {
-		//1- check that request exists and is in active 
-		// check that donor id matches request donor id
-		// reset the request status
+
 	}
-	
+
+	/**
+	 * Public Constructor
+	 * 
+	 * @param complaintRepository
+	 * @param requestRepository
+	 */
+	public RequestCompletionCommandHandler(ComplaintRepository complaintRepository,
+			RequestRepository requestRepository) {
+		this.complaintRepository = complaintRepository;
+		this.requestRepository = requestRepository;
+	}
+
 	@Override
-	public Void handle(RequestCompletionCommand command) {
-		
+	public Response<Void> handle(RequestCompletionCommand command) {
+		Response<Void> response;
+		// 1- check that request exists and is in active
+		response = checkRequestExistsActive(command.requestId);
+		if (!response.errors.isEmpty()) {
+			return response;
+		}
+		Request request = requestRepository.findById(command.requestId).get();
 
-		return null;
+		// 2-check that donor id matches request donor id and donor is active
+		response = checkDonorAuthenticEligibility(request.getDonor(), command.donorId);
+		if (!response.errors.isEmpty()) {
+			return response;
+		}
+
+		// 3-reset the request status and save request
+		request.setIsActive(false);
+		requestRepository.save(request);
+
+		return new Response<Void>();
 
 	}
+
+	private Response<Void> checkDonorAuthenticEligibility(Donor donor, long donorId) {
+
+		if (donor.getId() != donorId) {
+			return new Response<Void>(Error.DonorRequestDontMatch);
+		}
+		if (donor.getDonorStatus() != DonorStatus.Active || donor.getDonorStatus() != DonorStatus.Inactive) {
+			return new Response<Void>(Error.IneligibleDonorStatus);
+		}
+		return new Response<Void>();
+	}
+
+	private Response<Void> checkRequestExistsActive(long requestId) {
+		Optional<Request> dbRequest = requestRepository.findById(requestId);
+		if (dbRequest.isEmpty()) {
+			return new Response<Void>(Error.RequestDoesNotExist);
+		}
+		Request request = dbRequest.get();
+		if (!request.getIsActive()) {
+			return new Response<Void>(Error.InactiveRequest);
+		}
+		return new Response<Void>();
+	}
+
 }
