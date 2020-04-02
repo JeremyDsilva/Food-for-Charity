cdf6bd0ced46e07b5f4165c26ebd67602d6c6627
Aghyad <aaa1997aaa@gmail.com>
2020-04-02 18:36:10 +0400

    modified PersonController and register

--- web/src/main/java/com/foodforcharity/app/web/controller/PersonController.java
+++ web/src/main/java/com/foodforcharity/app/web/controller/PersonController.java
@@ -9,6 +9,8 @@ import com.foodforcharity.app.usecase.account.login.LoginCommand;
 import com.foodforcharity.app.usecase.account.register.RegisterCommand;
 import com.foodforcharity.app.web.model.AuthenticationRequest;
 import com.foodforcharity.app.web.model.AuthenticationResponse;
+import com.foodforcharity.app.web.model.RequestModel;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -81,17 +84,17 @@ public class PersonController {
     }
     
     @PostMapping("/register")
-    public String register(@RequestBody RequestModel requestModel) throws ExecutionException{
+    public String register(@RequestBody RequestModel requestModel, Model model) throws ExecutionException{
         RegisterCommand command = new RegisterCommand(requestModel.getName(), requestModel.getPassword(),
-            requestModel.getEmail(), requestModel.getPersonRole(), requestModel.PhoneNumber(),
-                requestModel.city(), requestModel.address());
+            requestModel.getEmail(), requestModel.getPersonRole(), requestModel.getPhoneNumber(),
+                requestModel.getCity(), requestModel.getCountry(), requestModel.getAddress());
 
        Response<Void> response = mediator.publishAsync(command).get();
 
         if(response.success()){
-            return "home";
+            return "login";
         }
-        else return ;
+        else model.addAttribute("id", response.getError().get(0).getMessage());
     }
 
 }
--- web/src/main/java/com/foodforcharity/app/web/model/RequestModel.java
+++ web/src/main/java/com/foodforcharity/app/web/model/RequestModel.java
@@ -2,16 +2,89 @@ package com.foodforcharity.app.web.model;
 
 import java.io.Serializable;
 
+import com.foodforcharity.app.domain.constant.PersonRole;
+
 
 public class RequestModel implements Serializable {
 
 
-    private String username;
+    private String name;
     private String password;
     private String email;
-    private boolean personRole;
+    private String personRole;
     private String phoneNumber;
+    private String country;
+
+
     private String city;
     private String address;
+
+    public RequestModel(){
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setUsername(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public PersonRole getPersonRole() {
+        return PersonRole.valueOf(personRole);
+    }
+
+    public void setPersonRole(String personRole) {
+        this.personRole = personRole;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
     
 }
