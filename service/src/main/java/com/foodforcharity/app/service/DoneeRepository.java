daefe379bbd40e3424c24500958134bd694b3ae1
JeremyDsilva <jeremydsilva7@gmail.com>
2020-04-03 04:03:29 +0400

    Added query to donee and donor repos

--- service/src/main/java/com/foodforcharity/app/service/DoneeRepository.java
+++ service/src/main/java/com/foodforcharity/app/service/DoneeRepository.java
@@ -1,8 +1,11 @@
 package com.foodforcharity.app.service;
 
+import java.util.List;
+
 import com.foodforcharity.app.domain.entity.Donee;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface DoneeRepository extends CrudRepository<Donee, Long> {
+    List<Donee> findByDoneeName(String doneeName);
 }
--- service/src/main/java/com/foodforcharity/app/service/DonorRepository.java
+++ service/src/main/java/com/foodforcharity/app/service/DonorRepository.java
@@ -1,8 +1,11 @@
 package com.foodforcharity.app.service;
 
+import java.util.List;
+
 import com.foodforcharity.app.domain.entity.Donor;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface DonorRepository extends CrudRepository<Donor, Long> {
+    List<Donor> findByDonorName(String donorName);
 }
