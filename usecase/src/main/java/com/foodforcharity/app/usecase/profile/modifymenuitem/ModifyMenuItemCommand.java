fb93b7af2b42e628500a10a46d134ff0583a5c79
PriyankaPrakashChand <53374732+PriyankaPrakashChand@users.noreply.github.com>
2020-04-04 14:35:39 +0400

    modify menu use case added

--- usecase/src/main/java/com/foodforcharity/app/usecase/profile/modifymenuitem/ModifyMenuItemCommand.java
+++ usecase/src/main/java/com/foodforcharity/app/usecase/profile/modifymenuitem/ModifyMenuItemCommand.java
@@ -2,18 +2,21 @@ package com.foodforcharity.app.usecase.profile.modifymenuitem;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import com.foodforcharity.app.domain.constant.Allergen;
 import com.foodforcharity.app.domain.constant.Cuisine;
 import com.foodforcharity.app.domain.constant.MealType;
+import com.foodforcharity.app.domain.constant.SpiceLevel;
+import com.foodforcharity.app.domain.reponse.Response;
 import com.foodforcharity.app.mediator.Command;
 
-public class ModifyMenuItemCommand implements Command<Void> {
+public class ModifyMenuItemCommand implements Command<Response<Void>> {
 
 	long donorId;
 
 	long foodId;
-
+	Optional<String> foodName;
 	Optional<String> descriptionText;
 
 	Optional<Integer> mealForNPeople;
@@ -23,7 +26,8 @@ public class ModifyMenuItemCommand implements Command<Void> {
 	Optional<Integer> quantityAvailable;
 	Optional<Cuisine> cuisine;
 	Optional<MealType> mealType;
-	Optional<List<Allergen>> allergens;
+	Set<Allergen> allergens;
+	Optional<SpiceLevel> spiceLevel;
 
 	public ModifyMenuItemCommand() {
 
@@ -38,21 +42,23 @@ public class ModifyMenuItemCommand implements Command<Void> {
 	public ModifyMenuItemCommand(long donorId, long foodId) {
 		this.donorId = donorId;
 		this.foodId = foodId;
+		this.foodName= Optional.empty();
 		this.descriptionText = Optional.empty();
 		this.mealForNPeople = Optional.empty();
 		this.originalPrice = Optional.empty();
 		this.quantityAvailable = Optional.empty();
 		this.cuisine = Optional.empty();
 		this.mealType = Optional.empty();
-		this.allergens = Optional.empty();
+		this.allergens =ArrayList<Allergen>();
+		this.spiceLevel= Optional.empty();
 	}
 
 	/**
 	 * 
 	 * @param allergens
 	 */
-	public void setAllergens(List<Allergen> allergens) {
-		this.allergens = Optional.of(allergens);
+	public void setAllergens(Set<Allergen> allergens) {
+		this.allergens = allergens;
 	}
 
 	/**
@@ -104,5 +110,36 @@ public class ModifyMenuItemCommand implements Command<Void> {
 		this.mealType = Optional.of(mealTypes);
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	public Optional<String> getFoodName() {
+		return foodName;
+	}
+
+	/**
+	 * 
+	 * @param foodName
+	 */
+	public void setFoodName(String foodName) {
+		this.foodName = Optional.of(foodName);
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public Optional<SpiceLevel> getSpiceLevel() {
+		return spiceLevel;
+	}
+
+	/**
+	 * 
+	 * @param spiceLevel
+	 */
+	public void setSpiceLevel(SpiceLevel spiceLevel) {
+		this.spiceLevel = Optional.of(spiceLevel);
+	}
 
 }
--- usecase/src/main/java/com/foodforcharity/app/usecase/profile/modifymenuitem/ModifyMenuItemCommandHandler.java
+++ usecase/src/main/java/com/foodforcharity/app/usecase/profile/modifymenuitem/ModifyMenuItemCommandHandler.java
@@ -1,21 +1,112 @@
 package com.foodforcharity.app.usecase.profile.modifymenuitem;
 
+import com.foodforcharity.app.domain.reponse.Response;
 import com.foodforcharity.app.mediator.CommandHandler;
+import com.foodforcharity.app.service.FoodRepository;
 
+import java.util.Optional;
+
+import com.foodforcharity.app.domain.constant.Error;
+import com.foodforcharity.app.domain.entity.Food;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ModifyMenuItemCommandHandler implements CommandHandler<ModifyMenuItemCommand, Void> {
+public class ModifyMenuItemCommandHandler implements CommandHandler<ModifyMenuItemCommand, Response<Void>> {
+	FoodRepository foodRepository;
 
-	public ModifyMenuItemCommandHandler(){
-
+	/**
+	 * Public Constructor
+	 * 
+	 * @param foodRepository
+	 */
+	@Autowired
+	public ModifyMenuItemCommandHandler(FoodRepository foodRepository) {
+		this.foodRepository = foodRepository;
 	}
 
 	@Override
-	public Void handle(ModifyMenuItemCommand command) {
+	public Response<Void> handle(ModifyMenuItemCommand command) {
 
-		return null;
+		try {
 
+			// check that food id exists
+			Optional<Food> dbFood = foodRepository.findById(command.foodId);
+			if (dbFood.isEmpty()) {
+				return Response.of(Error.DonorDoesNotExist);
+			}
+			Food food = dbFood.get();
+
+			// check that donor and food's donor match
+			if (command.donorId == food.getDonor().getId()) {
+				return Response.of(Error.FoodsDonorMismatch);
+			}
+
+			Integer minimumValue = 0;
+
+			// check that original price is not less than 0
+			if (command.originalPrice.isPresent() && command.originalPrice.get() < minimumValue) {
+				return Response.of(Error.InvalidOriginalPrice);
+			}
+
+			// check that MealforN people is not less than or equal to 0
+			if (command.mealForNPeople.isPresent() && command.mealForNPeople.get() <= minimumValue) {
+				return Response.of(Error.InvalidMealSize);
+			}
+
+			// check that quantity available is not less than 0
+			if (command.quantityAvailable.isPresent() && command.quantityAvailable.get() < minimumValue) {
+				return Response.of(Error.InvalidQuantityAvailable);
+			}
+			// check that description is not empty
+			if (command.descriptionText.isPresent() && command.descriptionText.get().isBlank()) {
+				return Response.of(Error.InvalidFoodDescriptionText);
+			}
+
+			// check that foodname is not empty
+
+			if (command.foodName.isPresent() && command.foodName.get().isBlank()) {
+				return Response.of(Error.InvalidFoodName);
+			}
+
+			if (command.foodName.isPresent()) {
+				food.setFoodName(command.foodName.get());
+			}
+			if (command.descriptionText.isPresent()) {
+				food.setDescriptionText(command.descriptionText.get());
+			}
+			if (command.originalPrice.isPresent()) {
+				food.setPrice(command.originalPrice.get());
+			}
+			if (command.mealForNPeople.isPresent()) {
+				food.setMealForNPeople(command.mealForNPeople.get());
+			}
+			if (command.quantityAvailable.isPresent()) {
+				food.setQuantityAvailable(command.quantityAvailable.get());
+			}
+
+			if (command.cuisine.isPresent()) {
+				food.setCuisines(command.cuisine.get());
+			}
+			if (command.mealType.isPresent()) {
+				food.setMealType(command.mealType.get());
+				// method takes a set but we decided to make it a single value}
+				{
+					food.setSpiceLevel(command.spiceLevel.get());
+				}
+
+				food.setAllergens(command.allergens);
+
+				// save to foodRepository
+				foodRepository.save(food);
+
+			}
+		} catch (Exception e) {
+			return Response.of(Error.UnknownError);
+		}
+
+		return Response.EmptyResponse();
 	}
 
 }
