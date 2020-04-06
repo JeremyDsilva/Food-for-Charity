826d3da446ef960803aa2e73c6140c78cf319f07
JeremyDsilva <jeremydsilva7@gmail.com>
2020-04-06 04:51:26 +0400

    updated to using cookies

--- web/src/main/java/com/foodforcharity/app/web/filter/JwtTokenFilter.java
+++ web/src/main/java/com/foodforcharity/app/web/filter/JwtTokenFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import com.foodforcharity.app.service.PersonDetailsService;
@@ -22,6 +23,7 @@ import org.springframework.web.filter.GenericFilterBean;
  */
 public class JwtTokenFilter extends GenericFilterBean {
     private static final String BEARER = "Bearer";
+    private String COOKIENAME = "accesstoken";
 
     private PersonDetailsService personDetailsService;
 
@@ -31,31 +33,50 @@ public class JwtTokenFilter extends GenericFilterBean {
     }
 
     /**
-     * Determine if there is a JWT as part of the HTTP Request Header.
-     * If it is valid then set the current context With the Authentication(user and roles) found in the token
+     * Determine if there is a JWT as part of the HTTP Request Header. If it is
+     * valid then set the current context With the Authentication(user and roles)
+     * found in the token
      *
-     * @param req Servlet Request
-     * @param res Servlet Response
+     * @param req         Servlet Request
+     * @param res         Servlet Response
      * @param filterChain Filter Chain
      * @throws IOException
      * @throws ServletException
      */
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain)
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
             throws IOException, ServletException {
-        //Check for Authorization:Bearer JWT
-        String headerValue = ((HttpServletRequest)req).getHeader("Authorization");
-        getBearerToken(headerValue).ifPresent(token-> {
-            //Pull the Username and Roles from the JWT to construct the user details
-            personDetailsService.loadUserByJwtToken(token).ifPresent(userDetails -> {
-                //Add the user details (Permissions) to the Context for just this API invocation
-                SecurityContextHolder.getContext().setAuthentication(
-                        new PreAuthenticatedAuthenticationToken(userDetails, "", userDetails.getAuthorities()));
-            });
-        });
 
-        //move on to the next filter in the chains
-        filterChain.doFilter(req, res);
+        // Check for Authorization:Bearer JWT
+        getJwtFromCookie((HttpServletRequest) request).ifPresent(jwt -> getBearerToken(jwt).ifPresent(token ->
+        // Pull the Username and Roles from the JWT to construct the user details
+        personDetailsService.loadUserByJwtToken(token).ifPresent(userDetails -> {
+            // Add the user details (Permissions) to the Context for just this API
+            // invocation
+            SecurityContextHolder.getContext().setAuthentication(
+                    new PreAuthenticatedAuthenticationToken(userDetails, "", userDetails.getAuthorities()));
+        })));
+
+        // move on to the next filter in the chains
+        filterChain.doFilter(request, response);
+    }
+
+    private Optional<String> getJwtFromCookie(HttpServletRequest request) {
+        try {
+            Cookie[] cookies = request.getCookies();
+            for (Cookie cookie : cookies) {
+                if (COOKIENAME.equals(cookie.getName())) {
+                    String accessToken = cookie.getValue();
+                    if (accessToken == null)
+                        return Optional.empty();
+
+                    return Optional.of(accessToken);
+                }
+            }
+        } catch (Exception e) {
+
+        }
+        return Optional.empty();
     }
 
     /**
@@ -65,8 +86,8 @@ public class JwtTokenFilter extends GenericFilterBean {
      * @return jwt if present, empty otherwise
      */
     private Optional<String> getBearerToken(String headerVal) {
-        if (headerVal != null && headerVal.startsWith(BEARER)) {
-            return Optional.of(headerVal.replace(BEARER, "").trim());
+        if (headerVal != null) {
+            return Optional.of(headerVal);
         }
         return Optional.empty();
     }
