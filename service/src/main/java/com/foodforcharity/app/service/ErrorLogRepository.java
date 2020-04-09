3842a5ee1beddb28334168a4b778baef0faa8063
JeremyDsilva <jeremydsilva7@gmail.com>
2020-04-09 18:03:15 +0400

    changed to infrastructure

--- service/src/main/java/com/foodforcharity/app/service/ErrorLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.foodforcharity.app.service;
-
-
-import com.foodforcharity.app.domain.entity.ErrorLog;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ErrorLogRepository extends CrudRepository<ErrorLog, Long> {
-}
--- service/src/main/java/com/foodforcharity/app/service/RequestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.foodforcharity.app.service;
-
-import com.foodforcharity.app.domain.entity.Request;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RequestRepository extends CrudRepository<Request, Long> {   
-}
