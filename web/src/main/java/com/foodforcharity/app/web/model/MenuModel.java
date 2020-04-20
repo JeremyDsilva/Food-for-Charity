a477420fc7036adeb5a287eb32cfb7d250e6c87a
aghyad97 <aaa1997aaa@gmail.com>
2020-04-20 07:06:46 +0400

    edit menu view

--- web/src/main/java/com/foodforcharity/app/web/model/MenuModel.java
+++ web/src/main/java/com/foodforcharity/app/web/model/MenuModel.java
@@ -1,11 +1,15 @@
 package com.foodforcharity.app.web.model;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotNull;
 
 import com.foodforcharity.app.domain.constant.Allergen;
+import com.foodforcharity.app.domain.constant.Cuisine;
+import com.foodforcharity.app.domain.constant.MealType;
+import com.foodforcharity.app.domain.constant.SpiceLevel;
 
 import lombok.Data;
 
@@ -35,15 +39,15 @@ public class MenuModel extends Request{
     @Digits(integer = Integer.MAX_VALUE, fraction = 0, message = "The number should be digits")
     private int quantityAvailable;
     
-    @NotNull(message = "Meal for number of people cannot be empty")
+    @NotNull(message = "Spice level cannot be empty")
     @Digits(integer = Integer.MAX_VALUE, fraction = 0, message = "The number should be digits")
-    private String spiceLevel;
+    private List<SpiceLevel> spiceLevel;
     
     @NotNull(message = "Meal type cannot be empty")
-    private String mealType;
+    private List<MealType> mealTypes;
     
     @NotNull(message = "Cuisine cannot be empty")
-    private String cuisine;
+    private List<Cuisine> cuisines;
     
     @NotNull(message = "Allergen cannot be empty")
     private Set<Allergen> allergen;
