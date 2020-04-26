7dc0cecf3294c090a6eb5f2d0bfec1e972193c12
aghyad97 <aaa1997aaa@gmail.com>
2020-04-26 22:43:46 +0400

    add SeleniumTest

--- web/src/test/java/com/foodforcharity/app/web/ApplicationTests.java
+++ web/src/test/java/com/foodforcharity/app/web/ApplicationTests.java
@@ -2,7 +2,10 @@ package com.foodforcharity.app.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
