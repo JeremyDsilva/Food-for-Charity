f731c5eddd12dd97b7962a7037b85ac310fb061f
aghyad97 <aaa1997aaa@gmail.com>
2020-04-27 16:50:14 +0400

    make food request

--- web/src/main/java/com/foodforcharity/app/web/controller/DoneeController.java
+++ web/src/main/java/com/foodforcharity/app/web/controller/DoneeController.java
@@ -2,8 +2,11 @@ package com.foodforcharity.app.web.controller;
 
 import com.foodforcharity.app.domain.reponse.Response;
 import com.foodforcharity.app.mediator.Mediator;
+import com.foodforcharity.app.usecase.foodreservation.createrequest.CreateRequestCommand;
 import com.foodforcharity.app.usecase.profile.selectpreferences.SelectPreferencesCommand;
 import com.foodforcharity.app.web.model.FoodPreferences;
+import com.foodforcharity.app.web.model.FoodRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -12,7 +15,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import java.util.Optional;
 import java.util.concurrent.ExecutionException;
+
+import static com.foodforcharity.app.web.model.Request.withSuccess;
 
 @Controller
 @RequestMapping("/donee")
@@ -26,7 +33,30 @@ public class DoneeController extends AbstractController {
 
     @GetMapping("/home")
     public String getDoneeHomepageView() {
+
+        // Send fooddtos to the homepage
+
         return "donee/donee-homepage";
+    }
+
+    @PostMapping("/home")
+    public String makeFoodRequest(@RequestParam(value = "donorId", required = false) Optional<Long> donorId,
+            FoodRequest request, Model model) throws ExecutionException {
+
+        CreateRequestCommand command = new CreateRequestCommand(getPersonId(), donorId.get());
+
+        Response<Void> response = publishAsync(command).get();
+
+        if (response.hasError()) {
+            request.setError(response.getError());
+            return "redirect:/";
+        }
+
+        request.setSuccess(true);
+
+        model.addAttribute("success", withSuccess(request));
+
+        return "redirect:/";
     }
 
     // --------------View Profile----------------
@@ -57,7 +87,8 @@ public class DoneeController extends AbstractController {
     }
 
     @PostMapping(value = "/edit-food-preferences")
-    public String selectPreferences(@Valid FoodPreferences foodPreferences, BindingResult result, Model model) throws ExecutionException {
+    public String selectPreferences(@Valid FoodPreferences foodPreferences, BindingResult result, Model model)
+            throws ExecutionException {
 
         if (result.hasErrors()) {
             return "donee/food-preferences";
@@ -77,41 +108,45 @@ public class DoneeController extends AbstractController {
     }
 
     // @PutMapping("FoodPreferences")
-    // public String modifyPreferences(@ModelAttribute FoodPreferences foodPreferences, Model model){
+    // public String modifyPreferences(@ModelAttribute FoodPreferences
+    // foodPreferences, Model model){
 
-    //     Response<Void> response;
+    // Response<Void> response;
 
-    //     SelectPreferencesCommand selectPreferencesCommand = new SelectPreferencesCommand(getPersonId());
+    // SelectPreferencesCommand selectPreferencesCommand = new
+    // SelectPreferencesCommand(getPersonId());
 
-    //     response = publishAsync(selectPreferencesCommand).get();
+    // response = publishAsync(selectPreferencesCommand).get();
 
-    //     if(!response.success()){
-    //         model.addAttribute("IsError", true);
-    //         model.addAttribute("ErrorMessage", response.getError().getMessage());
-    //     }
-    //     else {
-    //         model.addAttribute("Success", "Food Preferences Modified Successfully!");
-    //     }
-    //     return "redirect:/";
+    // if(!response.success()){
+    // model.addAttribute("IsError", true);
+    // model.addAttribute("ErrorMessage", response.getError().getMessage());
+    // }
+    // else {
+    // model.addAttribute("Success", "Food Preferences Modified Successfully!");
+    // }
+    // return "redirect:/";
     // }
 
     // @DeleteMapping("FoodPreferences")
-    // public String deletePreferences(@ModelAttribute FoodPreferences foodPreferences, Model model){
+    // public String deletePreferences(@ModelAttribute FoodPreferences
+    // foodPreferences, Model model){
 
-    //     Response<Void> response;
+    // Response<Void> response;
 
-    //     SelectPreferencesCommand selectPreferencesCommand = new SelectPreferencesCommand(getPersonId());
+    // SelectPreferencesCommand selectPreferencesCommand = new
+    // SelectPreferencesCommand(getPersonId());
 
-    //     response = publishAsync(selectPreferencesCommand).get();
+    // response = publishAsync(selectPreferencesCommand).get();
 
-    //     if(!response.success()){
-    //         model.addAttribute("IsError", true);
-    //         model.addAttribute("ErrorMessage", response.getError().getMessage());
-    //     }
+    // if(!response.success()){
+    // model.addAttribute("IsError", true);
+    // model.addAttribute("ErrorMessage", response.getError().getMessage());
+    // }
 
-    //     model.addAttribute("Success", "Food Preferences Deleted Successfully!");
+    // model.addAttribute("Success", "Food Preferences Deleted Successfully!");
 
-    //     return "redirect:/";
+    // return "redirect:/";
     // }
 
     // --------------Food Requests----------------
---
+++ web/src/main/java/com/foodforcharity/app/web/model/FoodRequest.java
@@ -0,0 +1,10 @@
+package com.foodforcharity.app.web.model;
+
+public class FoodRequest extends Request{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+}
